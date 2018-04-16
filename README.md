9de1772fe968925eec1d647b873c665df6c911ce
MaryGrace Blair <mblair021@gmail.com>
2018-04-16 15:59:12 -0500

    starter files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Food Waste Monitoring App
+
+The goal of this web-app is to help consumers reduce their food waste. Consumers will be able to scan Dillons 
+receipts to mantain a virtual record of foods that they have purchased. Consumers will then take weekly consumption
+surveys before their next grocery trip. These surveys will ask the user to provide information about what they ate and
+what they wasted. The app will also provide graphs and visualizations of the user's food waste. In the future, I would
+like to add a recomendation system that will help user's make smarter purchases that will minimize their food waste. 
+
+1. Clone this
+2. Copy web/config/settings/secret.py.example to web/config/settings/secret.py
+3. `docker-compose build`
+4. `docker-compose up`
---
+++ web/config/settings/base.py
@@ -0,0 +1,96 @@
+import os
+from unipath import Path
+from .secret import Secret
+
+BASE_DIR = Path(__file__).ancestor(3)
+MEDIA_ROOT = "/media"
+STATIC_ROOT = "/static"
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+SECRET_KEY = Secret.SECRET_KEY
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '0.0.0.0'
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'apps.grocery'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR.child("templates"),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'food_app_db',
+        'PORT': 5432,
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Chicago'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
